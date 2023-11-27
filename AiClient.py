6b267cdea2da96a6ecdf7488791d71691cdc340a
Yuvaraj N <56192888+Yuvaraj2519@users.noreply.github.com>
2023-11-27 13:07:29 +0530

    Create AiClient.py
    initial commit

---
+++ AiClient.py
@@ -0,0 +1,35 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+
+
+def getresponse (prompt):
+
+        load_dotenv()
+        
+        api_version = os.environ['API_VERSION']
+        api_key = os.environ['API_KEY']
+        azure_endpoint = os.environ['AZURE_ENDPOINT']
+        azure_deployment = os.environ['AZURE_DEPLOYMENT']
+        client = AzureOpenAI(
+                api_version=api_version,
+                api_key=api_key,
+                azure_endpoint=azure_endpoint,
+                azure_deployment=azure_deployment
+        )
+        prompt = prompt
+        chat_completion = client.chat.completions.create (
+        messages=[
+                {
+                        "role": "user",
+                        "content": prompt,
+                        }
+                ], model="apt-3,5-turbo",
+        )
+        # print (os.environ ['API_VERSION:])
+        # print (chat_completion.id)
+        for choice in chat_completion.choices:
+           return choice.message.content
+
+query = input("Al client: how can i help you/nUser:")
+print ("AI Client: "+getresponse (query))
